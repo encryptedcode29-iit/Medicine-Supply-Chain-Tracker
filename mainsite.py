f63a7cef519a73e582e2171dbc5b169a5060f372
Arnav Anand Tiwari <arnavanandtiwari@gmail.com>
2026-02-17 17:04:26 +0530

    Update mainsite.py

--- mainsite.py
+++ mainsite.py
@@ -19,8 +19,10 @@ FIREBASE_API_KEY = os.getenv("FIREBASE_API_KEY")
 app = FastAPI()
 load_dotenv() 
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
 app.add_middleware(
     SessionMiddleware,
     secret_key = os.getenv("secret"),
